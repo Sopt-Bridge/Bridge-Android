862af70f761b1ec837ad0a52d97ce03d41be759f
ji <hsji123@naver.com>
2018-07-01 18:16:12 +0900

    메인구조잡기 진행중

---
+++ app/src/main/java/com/cow/bridge/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.cow.bridge
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    var prevMenuItem: MenuItem? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        main_bottom_navigation!!.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_home -> main_viewpager.setCurrentItem(0, false)
+                R.id.action_subscribe -> main_viewpager.setCurrentItem(1, false)
+                R.id.action_library -> main_viewpager.setCurrentItem(2, false);
+            }
+            false
+        }
+
+
+        prevMenuItem!!.isChecked = main_bottom_navigation.menu.getItem(0).isChecked
+
+        main_viewpager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                prevMenuItem?.let {
+                    prevMenuItem?.isChecked = false
+                    main_bottom_navigation!!.menu.getItem(position).isChecked = true
+                    prevMenuItem = main_bottom_navigation!!.menu.getItem(position)
+
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+        })
+
+        main_viewpager.setOnTouchListener { v, event -> true }
+
+
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        //val mainFragment = MainFragment()
+        //val favoriteFragment = FavoriteFragment()
+        //adapter.addFragment(mainFragment)
+        //adapter.addFragment(favoriteFragment)
+        main_viewpager.adapter = adapter
+    }
+
+    inner class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
+        private val mFragmentList = ArrayList<Fragment>()
+        override fun getItem(position: Int): Fragment {
+            return mFragmentList[position]
+        }
+
+        override fun getCount(): Int {
+            return mFragmentList.size
+        }
+
+        fun addFragment(fragment: Fragment) {
+            mFragmentList.add(fragment)
+        }
+
+    }
+}
