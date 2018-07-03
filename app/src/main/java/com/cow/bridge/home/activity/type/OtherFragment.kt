9cf61fe0f3bcbfdfbf611700850f11af3e035cdb
ji <hsji123@naver.com>
2018-07-03 20:31:44 +0900

    HOME 모든 탭 레이아웃구현 완료

--- app/src/main/java/com/cow/bridge/home/activity/type/OtherFragment.kt
+++ app/src/main/java/com/cow/bridge/home/activity/type/OtherFragment.kt
@@ -1,13 +1,22 @@
 package com.cow.bridge.home.activity.type
 
 
+import android.app.Dialog
+import android.content.DialogInterface
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 
 import com.cow.bridge.R
+import com.cow.bridge.home.adapter.OtherAdapter
+import com.cow.bridge.home.dialog.OrderbyDialog
+import kotlinx.android.synthetic.main.fragment_other.view.*
 
 
 /**
@@ -30,7 +39,34 @@ class OtherFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val convertView = inflater!!.inflate(R.layout.fragment_other, container, false)
 
+        with(convertView){
+            other_layout_orderby.setOnClickListener(object : View.OnClickListener{
+                override fun onClick(p0: View?) {
+                    val orderbyDialog : OrderbyDialog = OrderbyDialog(activity, other_text_orderby.text.toString())
+                    orderbyDialog.window.setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT))
+                    orderbyDialog.show()
+                    orderbyDialog.setOnDismissListener(object : DialogInterface.OnDismissListener{
+                        override fun onDismiss(dialog: DialogInterface?) {
+                            with(dialog as OrderbyDialog){
+                                orderby?.let {
+                                    other_text_orderby.text = it
+                                    //TODO : 해당 정렬순으로 리스트가져오기
+                                }
+                            }
+                        }
 
+                    })
+                }
+
+            })
+
+            val otherAdapter : OtherAdapter = OtherAdapter(context)
+
+            val llm : RecyclerView.LayoutManager = GridLayoutManager(context, 2)
+            other_recycler.layoutManager = llm
+            other_recycler.adapter = otherAdapter
+
+        }
 
         return convertView
     }
---
+++ app/src/main/java/com/cow/bridge/home/dialog/OrderbyDialog.kt
@@ -0,0 +1,74 @@
+package com.cow.bridge.home.dialog
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.Window
+import android.widget.Toast
+import com.cow.bridge.R
+import kotlinx.android.synthetic.main.dialog_orderby.*
+
+/**
+ * Created by jihaeseong on 2018. 7. 3..
+ */
+
+class OrderbyDialog(context: Context, orderby : String) : Dialog(context), View.OnClickListener {
+
+    var orderby : String? = null
+
+    init {
+        this.orderby = orderby;
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.dialog_orderby)
+
+        when(orderby){
+            "Upload date" -> orderby_button_uploaddate.setBackgroundColor(Color.parseColor("#DDDDDD"))
+            "View count" -> orderby_button_viewcount.setBackgroundColor(Color.parseColor("#DDDDDD"))
+            "Rating" -> orderby_button_rating.setBackgroundColor(Color.parseColor("#DDDDDD"))
+        }
+
+        orderby_button_uploaddate.setOnClickListener(this)
+        orderby_button_viewcount.setOnClickListener(this)
+        orderby_button_rating.setOnClickListener(this)
+        orderby_button_cancel.setOnClickListener(this)
+        orderby_button_confirm.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        Log.v("test", v?.id.toString() + " " + R.id.orderby_button_uploaddate)
+        when(v?.id){
+            R.id.orderby_button_uploaddate -> {
+                orderby = "Upload date"
+                orderby_button_uploaddate.setBackgroundColor(Color.parseColor("#DDDDDD"))
+                orderby_button_viewcount.setBackgroundColor(Color.parseColor("#FFFFFF"))
+                orderby_button_rating.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            }
+            R.id.orderby_button_viewcount -> {
+                orderby = "View count"
+                orderby_button_uploaddate.setBackgroundColor(Color.parseColor("#FFFFFF"))
+                orderby_button_viewcount.setBackgroundColor(Color.parseColor("#DDDDDD"))
+                orderby_button_rating.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            }
+            R.id.orderby_button_rating -> {
+                orderby = "Rating"
+                orderby_button_uploaddate.setBackgroundColor(Color.parseColor("#FFFFFF"))
+                orderby_button_viewcount.setBackgroundColor(Color.parseColor("#FFFFFF"))
+                orderby_button_rating.setBackgroundColor(Color.parseColor("#DDDDDD"))
+            }
+            R.id.orderby_button_cancel -> {
+                orderby = null
+                dismiss()
+            }
+            R.id.orderby_button_confirm -> dismiss()
+
+        }
+    }
+
+}
