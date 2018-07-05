99c9808637c6f854edd99526bdaabcd7c578b272
mingmingg <laura126@naver.com>
2018-07-06 00:51:23 +0900

    add icon in layout & match size

--- app/src/main/java/com/cow/bridge/contents/activity/videoContentsMain.kt
+++ app/src/main/java/com/cow/bridge/contents/activity/videoContentsMain.kt
@@ -2,12 +2,13 @@ package com.cow.bridge.contents.activity
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AppCompatActivity
 import android.widget.TabHost
 import com.cow.bridge.R
 import kotlinx.android.synthetic.main.video_contents.*
 
-class videoContentsMain :AppCompatActivity(){
+class videoContentsMain :FragmentActivity(){
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.video_contents)
@@ -16,9 +17,6 @@ class videoContentsMain :AppCompatActivity(){
 
         val tab_comments : TabHost.TabSpec = host.newTabSpec("tabComments")
         val tab_upnext : TabHost.TabSpec = host.newTabSpec("tabUpNext")
-
-        tab_comments.setContent(R.id.video_contents_RecyclerView)
-        tab_upnext.setContent(R.id.video_contents_RecyclerView)
 
         tab_comments.setIndicator("Comments")
         tab_upnext.setIndicator("Up Next")
