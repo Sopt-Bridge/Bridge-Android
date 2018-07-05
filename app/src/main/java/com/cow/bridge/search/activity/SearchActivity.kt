858b4f24ca8a4c8406e762d68efacc8004ecbeeb
ji <hsji123@naver.com>
2018-07-05 16:56:44 +0900

    검색 세부레이아웃 구현 완료

--- app/src/main/java/com/cow/bridge/search/activity/SearchActivity.kt
+++ app/src/main/java/com/cow/bridge/search/activity/SearchActivity.kt
@@ -3,36 +3,41 @@ package com.cow.bridge.search.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.cow.bridge.R
-import com.miguelcatalan.materialsearchview.MaterialSearchView
+import com.cow.bridge.search.searchlibrary.MaterialSearchView
 import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
+
+    var searchView : MaterialSearchView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        search_view.setVoiceSearch(false)
-        search_view.setSubmitOnClick(true)
-        search_view.setCursorDrawable(R.drawable.color_cursor_white)
+        searchView = findViewById(R.id.search_view)
 
-        search_view.showSearch()
+        searchView?.setVoiceSearch(false)
+        searchView?.setSubmitOnClick(true)
+        searchView?.setCursorDrawable(R.drawable.color_cursor_white)
 
 
-        search_view.setOnQueryTextListener(object : MaterialSearchView.OnQueryTextListener {
+        searchView?.setOnQueryTextListener(object : MaterialSearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                //TODO : 검색 결과 뿌려주기
+
                 return false
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                //TODO : 최근 검색 결과 보여주기
+
                 return false
             }
 
+
         })
 
-        search_view.setOnSearchViewListener(object : MaterialSearchView.SearchViewListener{
+        searchView?.showSearch(false)
+
+        searchView?.setOnSearchViewListener(object : MaterialSearchView.SearchViewListener{
             override fun onSearchViewClosed() {
 
 
@@ -49,7 +54,6 @@ class SearchActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        search_view.showSearch()
     }
 
     override fun onDestroy() {
