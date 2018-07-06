77e561cff988237f527f3fe630e941dc5434b804
ji <hsji123@naver.com>
2018-07-06 22:01:37 +0900

    구독 메인페이지 기능구현 완료

---
+++ app/src/main/java/com/cow/bridge/subscribe/activity/BestChannelActivity.kt
@@ -0,0 +1,16 @@
+package com.cow.bridge.subscribe.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.cow.bridge.R
+
+class BestChannelActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_best_channel)
+
+
+
+    }
+}
