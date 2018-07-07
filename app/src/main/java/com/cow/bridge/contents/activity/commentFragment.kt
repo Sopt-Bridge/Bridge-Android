1f4626f00aebc3da0744faee422fa3cb8208194b
mingmingg <laura126@naver.com>
2018-07-07 16:35:03 +0900

    first merge comment

--- app/src/main/java/com/cow/bridge/contents/activity/commentFragment.kt
+++ app/src/main/java/com/cow/bridge/contents/activity/commentFragment.kt
@@ -2,3 +2,6 @@ package com.cow.bridge.contents.activity
 
 class commentFragment {
 }
+object obj_video_contents {
+
+}
--- app/src/main/java/com/cow/bridge/contents/activity/videoContentsMain.kt
+++ app/src/main/java/com/cow/bridge/contents/activity/videoContentsMain.kt
@@ -8,28 +8,34 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import android.widget.Adapter
-import com.cow.bridge.MainActivity
+import android.widget.Button
+import android.widget.TextView
 import kotlinx.android.synthetic.main.video_contents.*
 import com.cow.bridge.R
-import com.cow.bridge.contents.adapter.videoContentsTabAdapter
 
 class videoContentsMain :AppCompatActivity(){
     private var tablayout : TabLayout?= null
     var viewPager : ViewPager?= null
+    var btn_library : Button ?= null
+    var btn_feedback : Button ?= null
+    var txt_recommand : TextView ?= null
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.video_contents)
-
+        //tab
         tablayout = video_contents_tab
         viewPager = video_contents_vp_viewpager
         tablayout!!.setupWithViewPager(viewPager)
+        //others
+        btn_library = video_contents_bt_library
+        btn_feedback = video_contents_bt_feedback
+        txt_recommand = video_contents_tv_recommand
     }
 
     private fun setupViewPager(viewPager: ViewPager){
         val adapter = ViewPagerAdapter(supportFragmentManager)
-        adapter.addFragment()
+        adapter.addFragment(commentFragment())
     }
 
     internal inner class ViewPagerAdapter(manager : FragmentManager) : FragmentPagerAdapter(manager){
