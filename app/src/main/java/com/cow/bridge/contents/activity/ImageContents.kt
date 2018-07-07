869617e0bf68b49a40e1959694366d88b683ffb3
SERIN <aloha_rini@naver.com>
2018-07-07 16:33:02 +0900

    첫 커밋

---
+++ app/src/main/java/com/cow/bridge/contents/activity/ImageContents.kt
@@ -0,0 +1,13 @@
+package com.cow.bridge.contents.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.cow.bridge.R
+
+class ImageContents : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_contents)
+    }
+}
