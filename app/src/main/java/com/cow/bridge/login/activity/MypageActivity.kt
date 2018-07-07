c2e81c803c5e728f35cc7b4db9a224179e7125e0
heechuu <gmltn2800@gmail.com>
2018-07-07 16:31:28 +0900

    mypage 레이아웃 구현완료

--- app/src/main/java/com/cow/bridge/login/activity/MypageActivity.kt
+++ app/src/main/java/com/cow/bridge/login/activity/MypageActivity.kt
@@ -10,59 +10,64 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.cow.bridge.R
 import java.util.ArrayList
 
 
 class MypageActivity : AppCompatActivity() {
 
-    lateinit var toolbar: Toolbar
 
+    lateinit var toolbar: Toolbar
+    lateinit var mypageItems : ArrayList<MypageItems>
+    lateinit var myAdapter: MypageAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mypage)
+        setContentView(R.layout.mypage_activity)
         toolbar = findViewById<View>(R.id.mypage_toolbar) as Toolbar //툴바설정
-        toolbar.setTitleTextColor(Color.parseColor("#000000"))
-        setSupportActionBar(toolbar)//액션바와 같게 만들어줌
 
         val recyclerView = findViewById<View>(R.id.mypage_write) as RecyclerView
-        val list = ArrayList<String>()
-        for (i in 0..19) {
-            list.add("Item =$i")
-        }
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = MypageAdapter(list)
 
+        mypageItems = ArrayList()
+        mypageItems.add(MypageItems("[Requests] 번역 오역 요청","2018.01.03"))
+        mypageItems.add(MypageItems("[Requests] 영상 문의","2018.01.03"))
+        mypageItems.add(MypageItems("[Requests] 오류 문의","2018.07.05"))
+
+
+        myAdapter = MypageAdapter(mypageItems)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = myAdapter
+        recyclerView.addItemDecoration(ItemDecoration())
 
     }
 
-    private inner class MypageAdapter(private val list: List<String>) : RecyclerView.Adapter<MypageViewHolder>() {
+    inner class MypageAdapter(var mypageItems: ArrayList<MypageItems>)
+        : RecyclerView.Adapter<MypageViewHolder>() {
 
-        override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): MypageViewHolder {
-            val view = LayoutInflater.from(viewGroup.context).inflate(android.R.layout.simple_list_item_1, viewGroup, false)
+        override fun onCreateViewHolder(parent: ViewGroup, viewtype: Int): MypageViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.mypage_item, parent, false)
             return MypageViewHolder(view)
         }
 
         override fun onBindViewHolder(viewHolder: MypageViewHolder, position: Int) {
-            val text = list[position]
-            viewHolder.title.text = text
+
+            viewHolder.mypageWriting.text = mypageItems[position].writing
+            viewHolder.mypageDate.text = mypageItems[position].date
         }
 
 
         override fun getItemCount(): Int {
-            return list.size
+            return mypageItems.size
         }
     }
 
     inner class MypageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var title: TextView
 
-        init {
-            title = itemView.findViewById<View>(android.R.id.text1) as TextView
-
-
-        }
+        var mypageWriting : TextView = itemView!!.findViewById<TextView>(R.id.writing)
+        var mypageDate : TextView = itemView!!.findViewById<TextView>(R.id.date)
 
     }
 }
+
---
+++ app/src/main/java/com/cow/bridge/login/activity/MypageItems.kt
@@ -0,0 +1,7 @@
+package com.cow.bridge.login.activity
+
+class MypageItems(
+        var writing: String,
+        var date: String
+
+)
