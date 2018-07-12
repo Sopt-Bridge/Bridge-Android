d09822714f47a1e7d3706c50e78cc259771e464e
ji <hsji123@naver.com>
2018-07-12 18:29:38 +0900

    스플래시 gif 적용

--- app/src/main/java/com/cow/bridge/SplashActivity.kt
+++ app/src/main/java/com/cow/bridge/SplashActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
@@ -12,14 +14,12 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        animation_view.imageAssetsFolder = "/bridge/app/src/main/assets"
-        animation_view.setAnimation("splash.json")
+        Glide.with(this@SplashActivity).load(R.drawable.bridge).asGif().diskCacheStrategy(DiskCacheStrategy.SOURCE).into(gif_view);
 
         Handler().postDelayed(Runnable {
             var intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 4300)
-
+        }, 2150)
     }
 }
